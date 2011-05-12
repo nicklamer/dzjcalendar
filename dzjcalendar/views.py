fea2e3f63299f62c58174977893ec34177a84c0f
nicklamer <nicklamer@gmail.com>
2011-05-12 17:52:49 +0200

    simple calendar view added.
    working urls:
    /
    /cal
    /dzjcalendar

--- dzjcalendar/views.py
+++ dzjcalendar/views.py
@@ -1,1 +1,15 @@
 # Create your views here.
+from django.template import Context, loader
+from django.http import HttpResponse
+from dzjcalendar.models import User
+
+def index(request):
+    return HttpResponse("Hello, world! You're at the dzjcalendar site.")
+
+def calendar(request):
+    user = User.objects.all()
+    t = loader.get_template('calendar.html')
+    c = Context({
+        'users_list': user
+        })
+    return HttpResponse(t.render(c))
--- urls.py
+++ urls.py
@@ -7,9 +7,11 @@ admin.autodiscover()
 urlpatterns = patterns('',
     # Example:
     # (r'^dzjsite/', include('dzjsite.foo.urls')),
-
+     (r'^dzjcalendar/$', 'dzjcalendar.views.index'),
+     (r'^cal$', 'dzjcalendar.views.calendar'),
+     (r'^$', 'dzjcalendar.views.index'),
     # Uncomment the admin/doc line below to enable admin documentation:
-     (r'^admin/doc/', include('django.contrib.admindocs.urls')),
+    # (r'^admin/doc/', include('django.contrib.admindocs.urls')),
 
     # Uncomment the next line to enable the admin:
      (r'^admin/', include(admin.site.urls)),
